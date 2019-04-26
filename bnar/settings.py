648836324d3c034f7a9a03ed82bd8a8d33f1a88d
Martin G <martin@gubri.eu>
2019-04-25 21:09:39 -0300

    configure scrapy

--- bnar/settings.py
+++ bnar/settings.py
@@ -16,7 +16,7 @@ NEWSPIDER_MODULE = 'bnar.spiders'
 
 
 # Crawl responsibly by identifying yourself (and your website) on the user-agent
-#USER_AGENT = 'bnar (+http://www.yourdomain.com)'
+USER_AGENT = 'Bot scraping para Wikimedia (+https://github.com/Framartin/scrapy_biblioteca_national_argentina)'
 
 # Obey robots.txt rules
 ROBOTSTXT_OBEY = True
@@ -27,7 +27,7 @@ ROBOTSTXT_OBEY = True
 # Configure a delay for requests for the same website (default: 0)
 # See https://doc.scrapy.org/en/latest/topics/settings.html#download-delay
 # See also autothrottle settings and docs
-#DOWNLOAD_DELAY = 3
+DOWNLOAD_DELAY = 0.5
 # The download delay setting will honor only one of:
 #CONCURRENT_REQUESTS_PER_DOMAIN = 16
 #CONCURRENT_REQUESTS_PER_IP = 16
